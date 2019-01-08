1b97934d02c963ccc688f4cfdd75d84ee4c7d798
Dan Rue <drue@therub.org>
2019-01-08 16:51:35 -0600

    Add beaglebone-black
    It's mostly working. Health check doesn't yet pass due to some timing issue
    between lava and u-boot. Next step: update u-boot on beaglebone-black and see
    if it fixes the issues.
    
    Signed-off-by: Dan Rue <drue@therub.org>

--- server-overlay/root/provision.sh
+++ server-overlay/root/provision.sh
@@ -12,3 +12,10 @@ lava-server manage device-types list | grep -q qemu || \
 
 lava-server manage devices list | grep -q qemu-01 || \
     lava-server manage devices add --device-type qemu --worker dispatcher qemu-01
+
+lava-server manage device-types list | grep -q beaglebone-black || \
+    lava-server manage device-types add beaglebone-black
+
+lava-server manage devices list | grep -q beaglebone-black-01 || \
+    lava-server manage devices add --device-type beaglebone-black --worker dispatcher beaglebone-black-01
+
