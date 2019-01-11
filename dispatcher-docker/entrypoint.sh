94f662e9d5ae3f4402e9fb2004f422d3eb20787c
Dan Rue <drue@therub.org>
2019-01-11 14:25:51 -0600

    Misc updates
    - Install ser2net from sid for max-connections support!
    - Mount NFS and add a docker volume for it.. though it's still not working
    - Add a local device-types file for bbb for troubleshooting
    
    Signed-off-by: Dan Rue <drue@therub.org>

--- dispatcher-docker/entrypoint.sh
+++ dispatcher-docker/entrypoint.sh
@@ -14,5 +14,7 @@ MASTER_URL=${MASTER_URL:-tcp://localhost:5556}
 [ -e /etc/lava-dispatcher/lava-slave ] && . /etc/lava-dispatcher/lava-slave
 
 /etc/init.d/ser2net start
+/etc/init.d/tftpd-hpa start
+/etc/init.d/nfs-kernel-server start
 
 exec /usr/bin/lava-slave --level "$LOGLEVEL" --log-file "$LOGFILE" --master "$MASTER_URL" --socket-addr $LOGGER_URL $IPV6 $ENCRYPT $MASTER_CERT $SLAVE_CERT $DISPATCHER_HOSTNAME
