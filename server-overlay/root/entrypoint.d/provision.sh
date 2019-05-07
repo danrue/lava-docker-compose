919bf331f7e100a5865a3d242918a0f81959e6e8
Dan Rue <dan.rue@linaro.org>
2019-05-07 12:28:28 -0500

    Fix provisioning when run from scratch
    Previous implementation of the provision.sh ran after the lava-master
    process starts, and relied on the master process creating the default
    worker named 'dispatcher'. Current implementation runs the entrypoint
    scripts before starting lava-master, creating an order of operations
    problem.
    
    Solve by explicitly creating the default dispatcher in the provision
    script.
    
    Fixes #2
    
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- server-overlay/root/entrypoint.d/provision.sh
+++ server-overlay/root/entrypoint.d/provision.sh
@@ -10,5 +10,8 @@ lava-server manage users list | grep -q admin || \
 lava-server manage device-types list | grep -q qemu || \
     lava-server manage device-types add qemu
 
+lava-server manage workers list | grep dispatcher || \
+    lava-server manage workers add dispatcher
+
 lava-server manage devices list | grep -q qemu-01 || \
     lava-server manage devices add --device-type qemu --worker dispatcher qemu-01
